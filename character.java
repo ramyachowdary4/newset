6832316e9421cf0021bdfe4eb8e455c82ccb51d5
ramyachowdary4 <41936857+ramyachowdary4@users.noreply.github.com>
2018-08-16 12:41:37 +0530

    Create character.java

---
+++ character.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0;
+		String st;
+	            Scanner sc=new Scanner(System.in);
+		st=sc.nextLine();
+		for(int i=0;i<st.length();i++)
+		{
+			if(st.charAt(i)==' ')
+			{
+				continue;
+				
+			}
+			 sum++;
+		}
+		   System.out.println(sum);
+	}
+}
